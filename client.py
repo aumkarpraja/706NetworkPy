2c625cbbe7bdf8832cfe59e189b92c74a5d9b6df
Sameer Mehta <sameer-mehta@outlook.com>
2016-12-02 18:46:44 -0500

    Hello world works

---
+++ client.py
@@ -0,0 +1,13 @@
+import socket
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+DATA = 'hello world'
+BUFFER_SIZE = 80
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((TCP_IP,TCP_PORT))
+sock.send(DATA)
+data = sock.recv(BUFFER_SIZE)
+sock.close()
+
+print 'Recieved data: ', data
--- hisCinema.py
+++ hisCinema.py
@@ -1,5 +1,19 @@
 import socket
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 40022
+TCP_IP = '127.0.0.1' #change to socket.gethostname()
+TCP_PORT = 5005 #change to 40022
+
 BUFFER_SIZE = 1024
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((TCP_IP,TCP_PORT))
+sock.listen(1)
+
+conn, addr = sock.accept()
+print 'Connection addresses: ', addr
+
+while 1:
+    data = conn.recv(BUFFER_SIZE)
+    if not data: break
+    print 'Recieved data: ', data
+    conn.send(data)
+conn.close()
