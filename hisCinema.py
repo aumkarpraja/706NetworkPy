b1644a9bb076feb3b8c52364acba0668a7f6c83b
Sameer Mehta <sameer-mehta@outlook.com>
2016-12-02 20:23:42 -0500

    Conn

--- hisCinema.py
+++ hisCinema.py
@@ -1,7 +1,7 @@
 import socket
 
 TCP_IP = '127.0.0.1' #change to socket.gethostname()
-TCP_PORT = 40022 #change to 40022
+TCP_PORT = 40024 #change to 40022
 
 BUFFER_SIZE = 1024
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,7 +19,8 @@ print 'Connection addresses: ', addr
 while 1:
     data = conn.recv(BUFFER_SIZE)
     if not data: break
-    if data='GET':
+    if data=='GET':
         print 'Sending..'
         data = file.read(BUFFER_SIZE)
+        conn.send(data)
 conn.close()
