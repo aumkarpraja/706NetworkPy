e1b6dbfe8e3916ded9bd526e00719fe0be461e37
Sameer Mehta <sameer-mehta@outlook.com>
2016-12-03 23:54:37 -0500

    Client

--- client.py
+++ client.py
@@ -3,21 +3,19 @@ import socket
 TCP_IP = '127.0.0.1'
 TCP_PORT = 40024
 BUFFER_SIZE = 1024
-
-# Step 1, send a request that you want the web page from hiscinema.com
-MESSAGE = "GET"
+Message = '1'
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
+s.send(Message)
+file = open('recv.mp4','wb')
 
 # Step 2 - Wait for data
-while 1:
+while True:
     data = s.recv(BUFFER_SIZE)
     while (data):
         print "Receiving..."
-        f.write(data)
-f.close()
+        file.write(data)
+file.close()
 print "Done Receiving"
 s.close()                # Clos
-print data
-
+#print data
--- herCDN.py
+++ herCDN.py
@@ -2,20 +2,26 @@ import socket
 import sys
 from socket import *
 
-def query():
-    sock = socket.socket()
-    TCP_IP = '127.0.0.1'
-    TCP_PORT = 40024
-    BUFFER_SIZE = 1024
-    socket.bind((TCP_IP,TCP_PORT))
-    sock.listen(10)
-    while True:
-        sc, addr = sock.accept()
-        file = open('FrontSquat.mp4','wb')
-        while True:
-            file.write(sf)
-            sf = sc.recv(BUFFER_SIZE)
-        file.close()
-        sc.close()
 
-    sock.close()
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 40024
+BUFFER_SIZE = 1024
+sock = socket(AF_INET, SOCK_STREAM)
+sock.bind((TCP_IP,TCP_PORT))
+sock.listen(10)
+while True:
+    conn, addr = sock.accept()
+    data = conn.recv(BUFFER_SIZE)
+    if data == '1':
+        file = open('FrontSquat.mp4','wb')
+    if data == '2':
+        file = open('FrontSquat.mp4','wb')
+
+    while True:
+        file.write(data)
+
+    file.close()
+    conn.close()
+
+sock.close()
